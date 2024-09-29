c1a30b85dc3b5e32fc4ef0f257da3da5fa6fe2d2
joBrady <brady.joe63@gmail.com>
2024-09-29 14:00:09 -0500

    Modified File & Syncing GIT

--- labOneSD/labOneSD.ino
+++ labOneSD/labOneSD.ino
@@ -1,14 +1,9 @@
-#include "WiFi.h"
+3define PIN_ANALOG_IN 4
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  Serial.println("Setup done");
 }
 
 void loop() {
@@ -16,5 +11,11 @@ void loop() {
   //digitalWrite(13, HIGH);
   //delay(1000);
   //digitalWrite(13, LOW)
-
+  int adcValue = analogRead(PIN_ANALOG_IN);
+  double voltage = (float)adcValue / 4095.0 * 3.3;
+  double RT = 10 * voltage / (3.3 - voltage);
+  double tempK = 1 / (1/(273.15 + 25) + log(Rt / 10)/3950.0);
+  double tempC = tempK - 273.15;
+  Serial.print("ADC value : %d, \tVoltage : %2fV, \tTemperature : %.2fC\n", adcValue, voltage, tempC);
+  delay(1000);
 }
